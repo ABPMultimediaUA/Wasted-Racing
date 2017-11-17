d361eff0d6b5207bb5808eb4aa28055fb8d65666
Luis González Aracil <lga42@alu.ua.es>
2017-11-17 14:55:00 +0100

    EventManager created and working

---
+++ Game/src/GameEvent/EventManager.cpp
@@ -0,0 +1,53 @@
+#include "EventManager.h"
+
+EventManager& EventManager::getInstance() {
+    static EventManager instance;
+    return instance;
+}
+
+void EventManager::init() {
+
+}
+
+void EventManager::update() {
+
+    while(!EventManager::eventQueue.empty()){
+        EventManager::processEvent(EventManager::eventQueue.front());
+        EventManager::eventQueue.pop();
+    }
+
+}
+
+void EventManager::close() {
+
+}
+
+void EventManager::addEvent(Event event) {
+
+    EventManager::eventQueue.push(event);
+
+}
+
+void EventManager::processEvent(Event event){
+
+    auto eventListenerList = EventManager::eventListenerMap.find(event.type)->second;
+
+    for(auto eventListener : eventListenerList) {
+        eventListener.listener(event.data);
+    }
+
+}
+
+void EventManager::addListener(EventListener eventListener){
+
+    auto iterator = EventManager::eventListenerMap.find(eventListener.listenerType);
+
+    //Check if there's already a list for our listener and if not, create it
+    if(iterator==EventManager::eventListenerMap.end()){
+        EventListenerList eventListenerList; //new empty list
+        iterator = EventManager::eventListenerMap.insert(Pair(eventListener.listenerType, eventListenerList)).first;
+    }
+
+    iterator->second.push_back(eventListener);
+
+}
