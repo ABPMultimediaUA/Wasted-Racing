2f8d148192d2b37ba8b5283dc81fcaca996024be
Alexei <alexei-117@hotmail.com>
2018-01-19 08:51:24 +0100

    Network babe

--- Game/src/GameServer/main.cpp
+++ Game/src/GameServer/main.cpp
@@ -1,23 +1,39 @@
 #include "../Game.h"
+#include <raknet/MessageIdentifiers.h>
+#include <raknet/RakPeerInterface.h>
+#include <raknet/RakNetTypes.h>
 #include <iostream>
 
-void holita(){
-	for(int i = 0; i < 1000000000; ++i){
+#define MAX_PLAYERS_PER_SERVER 4
+#define MAX_CONNECTIONS_ALLOWED 10
+#define CLIENT_PORT 60006
+#define SERVER_PORT 60005
 
-	}
+void initServer(){
+	//get instance of peer interface, which usually connects it all
+	Raknet::RakPeerInterface* peer = RakNet::RakPeerInterface::GetInstance();
+
+	peer->Startup(MAX_CONNECTIONS_ALLOWED, &SocketDescriptor(SERVER_PORT,0), 1);
+	peer->SetMaximumIncomingConnections(MAX_PLAYERS_PER_SERVER);
+
+
 }
 
 int main(){
 
-	std::cout << "Starting game..." << std::endl;
+	std::cout << "Starting server..." << std::endl;
 
-	//holita();
+	initServer();
+
+	for(;;) {
+
+	}
 	
-	Game game;
+	/*Game game;
 	
-	game.Run();
+	game.Run();*/
 	
-	std::cout << "Exiting game..." << std::endl;
+	std::cout << "Exiting server..." << std::endl;
 	
 	return 0;  
 
