ce55d453789b80983ab1c012c412c6c1a43f28b4
pabloliborra <p.lopez.iborra@gmail.com>
2018-02-05 21:42:43 +0100

    Algunos constructores

---
+++ RedPandaStudio/src/TLight.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "TEntity.h"
+
+class TLight : public TEntity{
+
+private:
+    
+
+public:
+
+    TLight() {}
+    ~TLight() {}
+
+    //Draw Methods
+    void beginDraw() {}
+    void endDraw() {}
+
+};
---
+++ RedPandaStudio/src/TMesh.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "TEntity.h"
+
+class TMesh : public TEntity{
+
+private:
+    
+
+public:
+
+    TMesh() {}
+    ~TMesh() {}
+
+    //Draw Methods
+    void beginDraw() {}
+    void endDraw() {}
+
+};
