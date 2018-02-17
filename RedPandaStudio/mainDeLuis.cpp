86b2b087505fa9e4c94d4a6834e67c81d1d87d16
Luis González Aracil <lga42@alu.ua.es>
2018-02-17 23:01:44 +0100

    New main added for trying shit

---
+++ RedPandaStudio/mainDeLuis.cpp
@@ -0,0 +1,18 @@
+#include <RedPanda.h>
+#include "src/Facade/EngineFacade.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <GL/glew.h>
+#include <SFML/Graphics.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+int main() {
+
+    std::cout << "HEY :D" << std::endl;
+
+    return 0;
+
+}
