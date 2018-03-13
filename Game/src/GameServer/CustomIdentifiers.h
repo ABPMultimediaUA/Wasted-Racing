c596c30028e1942f60ced716eb808c76d72d94d2
alexei-117 <alexei-117@hotmail.com>
2018-03-13 18:11:38 +0100

    ALA

--- Game/src/GameServer/CustomIdentifiers.h
+++ Game/src/GameServer/CustomIdentifiers.h
@@ -2,14 +2,18 @@
 #include <raknet/MessageIdentifiers.h>
 
 enum customMessages {
+    //Initial raknet variables
+    ID_GAME_START = ID_USER_PACKET_ENUM,
+    //Game related
+    ID_GAME_ENDED,    
     ID_ACCUMULATED_TIME,
+    //Player management
     ID_PLAYERS_POSITION,
     ID_INPUT,
-    ID_GAME_START = ID_USER_PACKET_ENUM,
-    ID_GAME_ENDED,    
     ID_CREATE_PLAYER,
     ID_CREATE_REMOTE_PLAYER,
     ID_REMOTE_PLAYER_MOVEMENT,
+    //Item management
     ID_BOX_COLLISION,
     ID_CREATE_BANANA,
     ID_DESTROY_BANANA,
